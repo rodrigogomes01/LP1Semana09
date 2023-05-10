00946ebe600a682c3fd3ca708ae9782cb3a5f654
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-05-10 20:54:22 +0100

    Exercise 2
    Tested the program with new Program class

--- AnimalPerspective/Dog.cs
+++ AnimalPerspective/Dog.cs
@@ -4,7 +4,7 @@ namespace AnimalPerspective
 {
     public class Dog : Animal, IMammal
     {
-        public int NumberOfNipples { get {return 6;} }
+        public int NumberOfNipples { get {return 8;} }
         public override string Sound()
         {
             return base.Sound() + "Woof!";
--- AnimalPerspective/Program.cs
+++ AnimalPerspective/Program.cs
@@ -2,11 +2,52 @@
 
 namespace AnimalPerspective
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            const int n = 10;
+            Animal[] animals = new Animal[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                animals[i] = rnd.Next(4) switch
+                {
+                    0 => new Bat(),
+                    1 => new Bee(),
+                    2 => new Cat(),
+                    3 => new Dog(),
+                    _ => throw new ArgumentOutOfRangeException("Unknown animal!")
+                };
+            }
+
+            foreach (Animal a in animals)
+            {
+                Console.Write($"{a.GetType().Name} says ");
+                Console.Write(a.Sound());
+                if (a is IMammal)
+                    Console.Write(
+                        $", No.Nipples = {(a as IMammal).NumberOfNipples}");
+                if (a is ICanFly)
+                    Console.Write(
+                        $", No.Wings = {(a as ICanFly).NumberOfWings}");
+                Console.WriteLine();
+            }
+
+            // Possible output:
+
+            // Dog says Sound will be Woof!, No.Nipples = 8
+            // Dog says Sound will be Woof!, No.Nipples = 8
+            // Bat says Sound will be <Click>, No.Nipples = 2, No.Wings = 2
+            // Dog says Sound will be Woof!, No.Nipples = 8
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Dog says Sound will be Woof!, No.Nipples = 8
+
         }
     }
 }
