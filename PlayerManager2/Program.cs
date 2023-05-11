1db3d5ce17d5319400de91e9e887fccb7f072e49
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-05-11 16:38:42 +0100

    Exercise 4
    Added project PlayerManager2

---
+++ PlayerManager2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayerManager2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
